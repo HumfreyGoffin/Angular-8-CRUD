b7674bab831187e6ea47da56fafce5373c6ed6a8
Maxime Orione <maxpower98@hotmail.com>
2021-01-28 09:19:52 +0100

    Max: ✅ Finished up last steps of Cypress test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,14 +2,17 @@
 
 context('workspace-project App', () => {
   before(() => {
+    cy.viewport(1200, 800);
     cy.visit('http://localhost:4200');
   });
 
   it('sanity', () => {
+    // Log in
     cy.get('#username').type('admin@yopmail.com');
     cy.get('#password').type('admin123');
     cy.get('#login-button').click();
 
+    // Assert Table Headers
     const expectedTableHeaders = ['Sr. No.', 'First Name', 'Last Name', 'Email', 'Phone', 'Update', 'Delete'];
     cy.get('#student-table > tr')
       .first()
@@ -18,6 +21,7 @@ context('workspace-project App', () => {
       expect(th).to.contain(expectedTableHeaders[index]);
     });
 
+    // Assert Table Values
     const expectedTableValues = [
       ['1', 'Sangwin', 'Gawande', 'sangwin@yopmail.com', '+91-9503733178', "Update", 'Delete'],
       ['2', 'Oman', 'Umir', 'oman@yopmail.com', '+91-8574889658', "Update", 'Delete'],
@@ -31,7 +35,35 @@ context('workspace-project App', () => {
         expect(td).to.match('td');
         expect(td).to.contain(expectedRowValues[tdIndex]);
       });
-    })
+    });
+
+    // New Student
+    cy.get('#new-student-button').click();
+    cy.get('#first-name').type('Albert');
+    cy.get('#last-name').type('Einstein');
+    cy.get('#email').type('Albert.Einstein@gmail.com');
+    cy.get('#phone').type('9999999999');
+    cy.get('#submit-new-student').click();
+
+    // Assert New Table Values
+    const expectedNewTableValues = [
+      ['1', 'Albert', 'Einstein', 'Albert.Einstein@gmail.com', '+91-9999999999', "Update", 'Delete'],
+      ['2', 'Sangwin', 'Gawande', 'sangwin@yopmail.com', '+91-9503733178', "Update", 'Delete'],
+      ['3', 'Oman', 'Umir', 'oman@yopmail.com', '+91-8574889658', "Update", 'Delete'],
+      ['4', 'Tina', 'Dillon', 'tina@yopmail.com', '+91-7485889658', "Update", 'Delete'],
+      ['5', 'John', 'Doe', 'john@yopmail.com', '+91-9685589748', "Update", 'Delete'],
+      ['6', 'Peter', 'Parker', 'peter@yopmail.com', '+91-8595856547', "Update", 'Delete']
+    ];
+    expectedNewTableValues.forEach((expectedRowValues, rowValueIndex) => {
+      cy.get('#student-table > tr').eq(rowValueIndex + 1)
+        .children().each((td, tdIndex) => {
+        expect(td).to.match('td');
+        expect(td).to.contain(expectedRowValues[tdIndex]);
+      });
+    });
+
+    // Log Out
+    cy.get('#logout').click();
   });
 
 });
